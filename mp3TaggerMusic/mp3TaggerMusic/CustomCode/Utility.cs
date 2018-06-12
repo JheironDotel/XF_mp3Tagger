e719d7350b69b7089da8229924eadcb32f21c128
JheironDotel <jheirondotel@gmail.com>
2018-06-12 19:59:26 -0400

    Agregando archivo de configuracion

--- mp3TaggerMusic/mp3TaggerMusic/CustomCode/Utility.cs
+++ mp3TaggerMusic/mp3TaggerMusic/CustomCode/Utility.cs
@@ -2,16 +2,18 @@
 using mp3TaggerMusic.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Xamarin.Forms;
 
 namespace mp3TaggerMusic.CustomCode
 {
     public static class Utility
     {
-        public static string lastfmApiKey = "9b1e45575aa97afc4f34665f46148cc5";
+        public static string lastfmApiKey = GetLastFmApiKey(); //"9b1e45575aa97afc4f34665f46148cc5";
         //public static string lastfmUrl = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key={0}&artist={1}&track={2}&format=json";
         public static string lastfmUrl = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key={0}&{1}{2}&format=json";
 
@@ -76,6 +78,41 @@ namespace mp3TaggerMusic.CustomCode
         public static void Hide()
         {
             DependencyService.Get<IProgressInterface>().Hide();
+        }
+
+        public static string GetLastFmApiKey()
+        {
+            return GetBaseKey("lastfm-api-key");
+        }
+
+        static string GetBaseKey(string configKey)
+        {
+            XDocument doc = XDocument.Load("appSettings.config.xml");
+            var configKeyValue = doc.Element("config").Element(configKey).Value;
+                                      //select new
+                                      //{
+                                      //    Title = s.Attribute("Title").Value,
+                                      //    Artist = s.Attribute("Artist").Value,
+                                      //    TrackId = s.Attribute("TrackId").Value,
+                                      //    Timestamp = s.Attribute("Timestamp").Value
+                                      //};
+
+
+            return configKeyValue;
+
+            //var assembly = mp3TaggerMusic.GetTypeInfo(typeof(LoadResourceText)).Assembly;
+            //Stream stream = assembly.GetManifestResourceStream("WorkingWithFiles.PCLTextResource.txt");
+
+            //var type = typeof(Setup);
+            //var resource = string.Format("{0}.config.xml", type.Namespace);
+
+            //using (var stream = type.Assembly.GetManifestResourceStream(resource))
+            //using (var reader = new StreamReader(stream))
+            //{
+            //    var doc = XDocument.Parse(reader.ReadToEnd());
+
+            //    return doc.Element("config").Element(configKey).Value;
+            //}
         }
     }
 
