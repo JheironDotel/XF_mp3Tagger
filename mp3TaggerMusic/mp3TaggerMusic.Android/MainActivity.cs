d23e56841059915561e65ac4c837dcda8e6ab687
Jheiron Dotel <jdotel@statetrustlife.com>
2018-08-23 12:23:09 -0400

    working

--- mp3TaggerMusic/mp3TaggerMusic.Android/MainActivity.cs
+++ mp3TaggerMusic/mp3TaggerMusic.Android/MainActivity.cs
@@ -22,6 +22,8 @@ namespace mp3TaggerMusic.Droid
             global::Xamarin.Forms.Forms.Init(this, bundle);
             
             LoadApplication(new App());
+
+            
         }
     }
 }
